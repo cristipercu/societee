37f5bc3c031e7fc2290ca17982047860673ed421
Cristian Percu <percucristian@gmail.com>
2024-10-08 23:24:42 +0300

    batman

---
+++ fsocietee/layout/Navbar.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+  }
+</style>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <Link to="/">
+    <span class="navbar-brand">Societee</span>
+    </Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link to="/rules">
+          <span class="nav-link active" aria-current="page" >Rules</span>
+          </Link>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        Have fun!
+      </span>
+    </div>
+  </div>
+</nav>
---
+++ fsocietee/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Navbar from "../layout/Navbar.svelte";
+    import Home from "../pages/Home.svelte"
+    import { Router, Link, Route } from "svelte-routing";
+</script>
+
+<Router>
+<main>
+  <Navbar />
+  <div class="container mt-3 d-flex justify-content-center">
+  <Route path="/" component={Home} />
+  </div>
+</main>
+</Router>
+
+<style>
+</style>
